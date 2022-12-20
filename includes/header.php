ee79fe9b6e4896dfefd6f2005f6a9faeec0caefa
JeremyBeukema <jeremy-beukema@hotmail.com>
2022-12-20 15:20:18 +0100

    Css styles

--- includes/header.php
+++ includes/header.php
@@ -2,42 +2,37 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Facature zoeken</title>
-    <link rel="stylesheet" href="./css/styles.css">
-    <script src="./js/script.js"></script>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Facature zoeken</title>
+  <link rel="stylesheet" href="./css/styles.css">
+  <script src="./js/script.js"></script>
 </head>
-<body>
-    <?php include "conn.php" ?>
-<!-- Navbar comes here -->
-<nav>
-  <ul>
-    <div class="language js-language-picker">
-        
-  <form action="" class="language-picker__form">
-    <select name="language-picker-select" id="language-picker-select">
-      <option lang="de" value="deutsch">Deutsch</option>
-      <option lang="en" value="english" selected>English</option>
-      <option lang="fr" value="francais">Français</option>
-      <option lang="it" value="italiano">Italiano</option>
-      <!-- other language options -->
-    </select>
-  </form>
-</div>
 
-<div class="nav">
-<div class="logo">
-    <img src="./images/logo.png" alt="Logo">
-  </div>
-    <nav class="navMenu">
+<body>
+  <?php include "conn.php" ?>
+  <!-- Navbar comes here -->
+  <div class="nav">
+    <div class="logo">
+      <img src="./images/logo.png" alt="Logo">
+    </div>
+    <div class="language js-language-picker">
+      <form action="" class="language-picker__form">
+        <select name="language-picker-select" id="language-picker-select">
+          <option lang="de" value="deutsch">Deutsch</option>
+          <option lang="en" value="english" selected>English</option>
+          <option lang="fr" value="francais">Français</option>
+          <option lang="it" value="italiano">Italiano</option>
+          <!-- other language options -->
+        </select>
+      </form>
+    </div>
+    <div class="navMenu">
       <a href="#">File</a>
       <a href="#">View</a>
       <a href="#">Account</a>
       <a href="#">Inloggen</a>
       <div class="dot"></div>
-</div>
-    </nav>
-  </ul>
-</nav>
+    </div>
+  </div>
