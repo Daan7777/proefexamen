ef002aa04cdb8c5eb25b380e9f456597315a3388
Daan7777 <Detvanmontfoort@gmail.com>
2022-12-22 14:25:32 +0100

    account begin gemaakt

--- account.php
+++ account.php
@@ -1,6 +1,6 @@
 <?php 
 include "includes/header.php"; 
-
+/*
 // Initialize the session
 session_start();
  
@@ -8,15 +8,31 @@ session_start();
 if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
     header("location: login.php");
     exit;
-}
+} */
 ?>
 
 <div class="content">
   <!-- Back to top button -->
   <button onclick="topFunction()" id="myBtn" title="Go to top" style="font-size:200%; width:90px; height:90px;">^</button>
 
-  <h1 class="my-5">Hi, <b><?php echo htmlspecialchars($_SESSION["username"]); ?></b>. Welcome to our site.</h1>
-
+    <div class="edit">
+    <h1 style="padding:10px;">Edit</h1>
+    <form action="#" method="post">
+        <label for="">Your name</label><br>
+        <input type="text" name="firstName" id="firstName">
+        <br>
+        <label for="">Email</label><br>
+        <input type="email" name="email" id="email">
+        <br>
+        <label for="">Password</label><br>
+        <input type="password" name="password" id="password">
+        <br>
+        <label for="">Password</label><br>
+        <input type="password" name="password" id="password">
+        <br>
+        <input type="submit" value="Edit account">
+    </form>
+    </div>
 
 
 </div>
