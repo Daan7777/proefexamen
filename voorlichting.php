fd9fc0d2d37acb491da0885fea9b2a4f75e8453a
JeremyBeukema <jeremy-beukema@hotmail.com>
2022-12-22 14:25:54 +0100

    Small css fixes

--- voorlichting.php
+++ voorlichting.php
@@ -4,11 +4,11 @@
     <!-- Page content -->
     <!-- Back to top button -->
     <button onclick="topFunction()" id="myBtn" title="Go to top" style="font-size:200%; width:90px; height:90px;">^</button>
-
-    <h1 style="padding:70px;" >voorlichting</h1>
-    <p style="padding:20px;">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce vel diam ligula. Nunc rutrum elit non nibh blandit, in scelerisque odio viverra. Nunc condimentum pharetra odio a ultricies. In auctor bibendum ipsum, at tincidunt dui convallis ac. Nunc at metus vitae arcu volutpat efficitur vel nec massa. Nullam sit amet metus dapibus, suscipit orci ut, volutpat turpis. Praesent a feugiat velit. Fusce vel arcu dolor. Phasellus mollis euismod lectus, id dapibus diam viverra sit amet. Donec vitae fringilla ex. </p>
-    <p style="padding:20px;">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce vel diam ligula. Nunc rutrum elit non nibh blandit, in scelerisque odio viverra. Nunc condimentum pharetra odio a ultricies. In auctor bibendum ipsum, at tincidunt dui convallis ac. Nunc at metus vitae arcu volutpat efficitur vel nec massa. Nullam sit amet metus dapibus, suscipit orci ut, volutpat turpis. Praesent a feugiat velit. Fusce vel arcu dolor. Phasellus mollis euismod lectus, id dapibus diam viverra sit amet. Donec vitae fringilla ex. </p>
-
+        <div class="textPage">
+            <h1 style="padding:70px; font-size: 200%;" >voorlichting</h1>
+            <p style="padding:20px;">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce vel diam ligula. Nunc rutrum elit non nibh blandit, in scelerisque odio viverra. Nunc condimentum pharetra odio a ultricies. In auctor bibendum ipsum, at tincidunt dui convallis ac. Nunc at metus vitae arcu volutpat efficitur vel nec massa. Nullam sit amet metus dapibus, suscipit orci ut, volutpat turpis. Praesent a feugiat velit. Fusce vel arcu dolor. Phasellus mollis euismod lectus, id dapibus diam viverra sit amet. Donec vitae fringilla ex. </p>
+            <p style="padding:20px;">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce vel diam ligula. Nunc rutrum elit non nibh blandit, in scelerisque odio viverra. Nunc condimentum pharetra odio a ultricies. In auctor bibendum ipsum, at tincidunt dui convallis ac. Nunc at metus vitae arcu volutpat efficitur vel nec massa. Nullam sit amet metus dapibus, suscipit orci ut, volutpat turpis. Praesent a feugiat velit. Fusce vel arcu dolor. Phasellus mollis euismod lectus, id dapibus diam viverra sit amet. Donec vitae fringilla ex. </p>
+        </div>
 
 </div>
 <?php include "includes/footer.php" ?>
